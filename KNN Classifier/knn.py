03a5983d4ecac4fae6ee0380bc583082eade528e
Andres Restrepo <arestre1@msudenver.edu>
2020-03-15 17:43:14 -0600

    Clean up and refactoring for simplicity.

--- KNN Classifier/knn.py
+++ KNN Classifier/knn.py
@@ -5,6 +5,7 @@ from sklearn import preprocessing
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
+from sklearn import metrics
 
 def reading_data():   
     data = []
@@ -32,51 +33,32 @@ def encoding_data(x,y):
     y = label_encoder.fit_transform(y)
     return (x,y)
 
-def test_percentages(training, validation, testing, data_size):
-    train_size = data_size*training
-    validation_size = data_size*validation
-    partition_size = train_size + validation_size
-    ratio_1 = testing
-    ratio_2 = validation_size/partition_size
-    return  (ratio_1, ratio_2)
-    
-
 def train_validation_test(x,y, train_ratio, val_ratio, test_ratio):
     second_ratio = (val_ratio)/(train_ratio + val_ratio)
     train_x, test_x, train_y, test_y = train_test_split(x,y, test_size = test_ratio, random_state = 27)
-    train_x_val, test_x_val, train_y_val, test_y_val = train_test_split(train_x,train_y, test_size = second_ratio, random_state = 27)
-    return train_x, test_x, train_y, test_y, train_x_val, test_x_val, train_y_val, test_y_val
+    train_x, val_x, train_y, val_y= train_test_split(train_x,train_y, test_size = second_ratio, random_state = 27)
+    return train_x, test_x, train_y, test_y, val_x, val_y
 
+def metrics_score(y_test, y_pred): 
+    print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+    print("Precision:",metrics.precision_score(y_test, y_pred, average= 'weighted'))
+    print("Recall:",metrics.recall_score(y_test, y_pred, average= 'weighted'))
+    print("F Score:",metrics.f1_score(y_test,y_pred, average = 'weighted'))
 
-def knn_classifier(train_x, test_x, train_y, test_y, train_x_val, test_x_val, train_y_val, test_y_val, neighbors, metric):
-    cats = 0
-    dogs = 0
-    pandas = 0
-
-    model = KNeighborsClassifier(n_neighbors=neighbors, p=metric)
-    model.fit(train_x, train_y)
-    for i in range(len(test_x)):
-        prediction = model.predict([test_x[i]])
-        if prediction == [0]:
-            cats  +=1
-        if prediction == [1]:
-            dogs  +=1
-        if prediction == [2]:
-            pandas +=1
-    print("Cats={}, Dogs={}, Pandas= {}".format(cats,dogs,pandas))
-
-def knn_validation(train_x_val, test_x_val, train_y_val, test_y_val):
-
-    neighbors = [3,5,7]
-    metrics = [1,2]
-    for neighbors in neighbors:
+def knn(train_x, test_x, train_y, test_y, neighbors, metrics):
+    for neighbor in neighbors:
         for metric in metrics:
-            model = KNeighborsClassifier(n_neighbors=neighbors, p=metric)
-            model.fit(train_x_val, train_y_val)
+            model = KNeighborsClassifier(n_neighbors=neighbor, p=metric)
+            model.fit(train_x, train_y)
+            y_predict = model.predict(test_x)
+            print("Number of neighbors= {}, L= {}".format(neighbor, metric))
+            metrics_score(test_y, y_predict)
+            print()
 
 x,y = reading_data()
 x,y = loading_data(x,y)
 x,y = encoding_data(x,y)
-train_x, test_x, train_y, test_y, train_x_val, test_x_val, train_y_val, test_y_val = train_validation_test(x,y, 0.7, 0.1, 0.2)
-knn_classifier(train_x, test_x, train_y, test_y, train_x_val, test_x_val, train_y_val, test_y_val, 1, 2)
+train_x, test_x, train_y, test_y, val_x, val_y = train_validation_test(x,y, 0.7, 0.1, 0.2)
+knn(train_x, test_x, train_y, test_y, [7], [1])
+
 
